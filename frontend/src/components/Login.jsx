c954ea0e94a55379416de75a90a826e44cc24082
Stefan Talbot <satalbot@protonmail.com>
2023-04-22 15:25:58 -0400

    Feat: Change Login to Controlled Component
    * Add state to Login component form.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+
+  const [loginValues, setLoginValues] = useState({email: '', password: ''});
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Login')
+  };
+
   return (
     <div className="bg-slate-900 h-screen w-full flex justify-center items-center">
-      <form className="flex flex-col items-center">
+      <form className="flex flex-col items-center" onSubmit={e => handleSubmit(e)}>
         <span className="flex flex-row justify-end pb-4 w-full">
-          <label for="email" className="text-white font-main pr-2">E-mail</label>
+          <label className="text-white font-main pr-2">E-mail</label>
           <div className="border-l-4 w-2 bg-white border-violet-700"></div>
-          <input type="text" name="email" className="rounded-r-md focus:outline-none"></input>
+          <input className="rounded-r-md focus:outline-none" 
+            type="text" 
+            name="email"  
+            onChange={e => setLoginValues({...loginValues, email: e.target.value})} 
+            value={loginValues.email} />
         </span>
         <span className="flex flex-row justify-end pb-4">
-          <label for="password" className="text-white font-main pr-2">Password</label>
+          <label className="text-white font-main pr-2">Password</label>
           <div className="border-l-4 w-2 bg-white border-violet-700"></div>
-          <input type="password" name="password" className="rounded-r-md focus:outline-none"></input>
+          <input className="rounded-r-md focus:outline-none" 
+            type="password" 
+            name="password"  
+            onChange={e => setLoginValues({...loginValues, password: e.target.value})} 
+            value={loginValues.password} />
         </span>
         <button type="submit" className="w-24 h-6 bg-violet-700 rounded md text-white font-main hover:bg-violet-300">LOGIN</button>
       </form>
