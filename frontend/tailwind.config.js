e3c8b9f32c9bfe0d56d9704a414949fcc4f7ad5e
Stefan Talbot <satalbot@protonmail.com>
2023-04-22 12:07:19 -0400

    Feat: Create React App
    * Create React app.
    * Add Tailwindcss to project.
    * Add custom fonts to tailwind.config.js.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/components/**/*.jsx"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      logo: ['Iceberg', 'cursive'],
+      main: ['Kanit', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
