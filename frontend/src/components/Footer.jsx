c80244281f5372428b96b3b9ca3c6887614247cb
Stefan Talbot <satalbot@protonmail.com>
2023-04-22 12:54:38 -0400

    Feat: Footer
    * Add react icons package.
    * Create Footer component.
    * Style Footer component.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { GiCctvCamera } from 'react-icons/gi'
+
+function Footer() {
+  return (
+    <footer>
+      <div className="fixed bottom-0 w-full h-16 bg-violet-700 flex flex-row">
+        <div className="fixed bottom-0 left-6 w-24 h-24 bg-violet-700 rounded-full flex justify-center items-center">
+          <GiCctvCamera className="fill-white h-16 w-16"/>
+        </div>
+        <h2 className="font-logo text-4xl text-white ml-32">WATCHER</h2>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
