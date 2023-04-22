673b8465cc0b4e0f3ee9db96170f8369292a4237
Stefan Talbot <satalbot@protonmail.com>
2023-04-22 14:50:09 -0400

    Style: Form Inputs
    * Remove focus outline.
    * Add space before input field.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,11 +6,13 @@ function Login() {
       <form className="flex flex-col items-center">
         <span className="flex flex-row justify-end pb-4 w-full">
           <label for="email" className="text-white font-main pr-2">E-mail</label>
-          <input type="text" name="email" className="rounded-md border-l-4 border-violet-700"></input>
+          <div className="border-l-4 w-2 bg-white border-violet-700"></div>
+          <input type="text" name="email" className="rounded-r-md focus:outline-none"></input>
         </span>
         <span className="flex flex-row justify-end pb-4">
           <label for="password" className="text-white font-main pr-2">Password</label>
-          <input type="password" name="password" className="rounded-md border-l-4 border-violet-700"></input>
+          <div className="border-l-4 w-2 bg-white border-violet-700"></div>
+          <input type="password" name="password" className="rounded-r-md focus:outline-none"></input>
         </span>
         <button type="submit" className="w-24 h-6 bg-violet-700 rounded md text-white font-main hover:bg-violet-300">LOGIN</button>
       </form>
