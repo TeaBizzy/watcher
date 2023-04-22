7a08ce553a5e842a7ae17cb8333812846438fba4
Stefan Talbot <satalbot@protonmail.com>
2023-04-22 13:33:36 -0400

    Feat: Header Component
    * Create Header component.
    * Style Header component.

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="font-main text-l md:text-4xl px-4 text-white w-full h-16 md:px-12 bg-violet-700 flex flex-row justify-between items-center">
+        <h2>user@email.com</h2>
+        <button className="h-5/6 px-4 flex justify-center items-center rounded-md hover:bg-violet-300"> 
+          <h2>LOGOUT</h2>
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
