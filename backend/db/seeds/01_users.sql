47138a8b302a9fb4012c3ea5dcd31986a69224c7
Stefan Talbot <satalbot@protonmail.com>
2023-04-25 15:50:54 -0400

    Feat: POST /login Route & Login Credential Validation
    * Add Bcrypt to project.
    * Add route POST /login.
    * Modify get_user_by_email to take a parameterized inputs.
    * Modify users seed passwords to be encrypted.

--- backend/db/seeds/01_users.sql
+++ backend/db/seeds/01_users.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (email, password) VALUES ('satalbot@protonmail.com', 'password');
-INSERT INTO users (email, password) VALUES ('yana.mayzenger@boltanddash.ai', 'password');
+INSERT INTO users (email, password) VALUES ('satalbot@protonmail.com', '$2b$10$NjeXLC4auxvDbyq.Pd0x8.UTKupwoG2XJW3uTAvppeIsB/5ZL2hLS');
+INSERT INTO users (email, password) VALUES ('yana.mayzenger@boltanddash.ai', '$2b$10$NjeXLC4auxvDbyq.Pd0x8.UTKupwoG2XJW3uTAvppeIsB/5ZL2hLS');
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const cors = require('cors')
+const client = require('./db/connection.js');
 const app = express();
 const port = 3030;
+const parseSQL = require('./db/parse-sql.js');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 
 app.use(cors({origin: 'http://localhost:3000'}))
-
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Hello World')
 });
 
 app.listen(port, () => {
   console.log(`Watcher server listening on port ${port}`)
+})
+
+// Log User In
+app.post('/login', (req, res) => {
+  const credentials = {...req.body};
+  const getUserByEmail = parseSQL('db/queries/01_get-users-by-email.sql');
+
+  client.query(getUserByEmail, [credentials.email])
+    .then(data => {
+      return bcrypt.compare(credentials.password, data.rows[0].password)
+    })
+    .then(result => {
+      if(!result) return Promise.reject()
+      console.log(`Logged in successfully as ${credentials.email}`)
+      res.status(200)
+      res.send()
+    })
+    .catch(err => {
+      console.log('Invalid Email or Password')
+      res.status(400)
+      res.send('Invalid Email or Password')
+    })
 })
 
 module.exports = app;
