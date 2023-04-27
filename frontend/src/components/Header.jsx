8588af3d6695298b6a8079d5490fb064d5595a8a
Stefan Talbot <satalbot@protonmail.com>
2023-04-27 12:06:47 -0400

    Style: Header Email Styling
    * Change email to uppercase.
    * Fix bug in useFetchCamera 'currentUser is null' error.
    * Change email font.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,18 +9,21 @@ import React from "react";
 
 function Header(props) {
 
-  const { setCurrentUser, email } = props;
+  const { setCurrentUser, setCurrentCamera, email } = props;
 
   function handleLogOut(e) {
     e.preventDefault();
     axios.post('http://localhost:3030/api/logout', {}, {withCredentials: true})
-      .then(() => setCurrentUser(''))
+      .then(() => {
+        setCurrentCamera(-1);
+        setCurrentUser('');
+      })
   }
 
   return (
     <header>
-      <div className="fixed top-0 font-main text-l md:text-4xl px-4 text-white w-full h-16 md:px-12 bg-violet-700 flex flex-row justify-between items-center">
-        <h2>{email}</h2>
+      <div className="fixed top-0 font-logo text-l md:text-4xl px-4 text-white w-full h-16 md:px-12 bg-violet-700 flex flex-row justify-between items-center">
+        <h2>{email.toUpperCase()}</h2>
         <button 
           className="h-5/6 px-4 flex justify-center items-center rounded-md hover:bg-violet-300" 
           onClick={(e) => handleLogOut(e)}
