91f323e891b9f27cffad2333f25ffa7a64bc50c5
Stefan Talbot <satalbot@protonmail.com>
2023-04-27 11:48:05 -0400

    Style: Adjust Responsive Design of Notifications

--- frontend/src/components/CameraView.jsx
+++ frontend/src/components/CameraView.jsx
@@ -17,6 +17,7 @@ function CameraView(props) {
 
   return (
     <div className="max-h-screen w-full lg:w-3/4 mt-16 pt-4 pb-28 flex flex-col justify-start items-center">
+      {/* Back button for mobile view */}
       <div className="w-full px-4 lg:hidden flex items-center">
         <TfiBackLeft 
           className="fill-white pr-2 w-12 bg-violet-700 h-24 hover:bg-gray-300" 
@@ -29,7 +30,8 @@ function CameraView(props) {
           </span>
         </div>
       </div>
-      <div className="w-full h-full px-4 md:px-24">
+      {/* Player & Notifications */}
+      <div className="w-full h-2/3 px-4 md:px-24">
         {active ?
         <ReactPlayer 
           url={`http://localhost:3030/api/stream/${id}`}
@@ -44,8 +46,8 @@ function CameraView(props) {
         </div>
         }
       </div>
-      <div className="flex flex-col w-full px-4 mt-4 justify-between lg:flex-row items-center">
-        <div className={`w-full md:w-2/3 h-24 px-4 mb-4 md:mr-4 md:mb-0 border-t-8 bg-gray-600 border-violet-600 flex justify-center items-center rounded-b-md transition-opacity ${motion ? 'opacity-100' : 'opacity-0 duration-1000'}`}>
+      <div className="flex flex-col w-full h-1/4 px-4 mt-4 justify-between items-center lg:flex-row lg:items-start">
+        <div className={`w-full md:w-2/3 h-24 px-4 mb-4 lg:mr-4 border-t-8 bg-gray-600 border-violet-600 flex justify-center items-center rounded-b-md transition-opacity ${motion ? 'opacity-100' : 'opacity-0 duration-1000'}`}>
           <FaWalking  className="fill-white pr-2 h-12 w-12 lg:h-12 lg:w-12"/>
           <p className="lg:text-2xl text-lg font-main text-white whitespace-nowrap">Motion Detected</p>
         </div>
